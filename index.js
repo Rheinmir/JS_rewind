4eb90cb68e514cf6441c85804b38ef3a634d2ce1
Rheinmir <rheinmir@gmail.com>
2024-04-26 07:52:11 +0700

    end-rewind

--- index.js
+++ index.js
@@ -1,2 +1,96 @@
-console.log("Hello Rhein");
-console.log("JS is a cake piece");
+// console.log("Hello Rhein");
+// console.log("JS is a cake piece");
+
+// //Assign the string 'Rhein' to the variable 'name'.
+// var name = 'Rhein';
+
+// //Assigns the value 2 to the variable 'age'.
+// let age =2;
+
+// //Defines a constant named "likes" with the value 50,000.
+// const likes = 50000;
+
+// //Stores the number 20000 in the variable `likes`.
+// likes = 20000;
+
+
+// // Logs the number 12300000 to the console.
+// console.log(123e5);
+// // Prints the number 0.00123.
+// console.log(123e-5);
+
+// // Prints the smallest possible JavaScript number.
+// console.log(Number.MIN_VALUE);
+// // Prints the maximum value a JavaScript number can represent.
+// console.log(Number.MAX_VALUE);
+
+// // Prints the largest BigInt representation in JavaScript.
+// console.log(
+//     BigInt("9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999"),
+// );
+
+
+// // Defines and prints strings using single, double, and backtick quotes, highlighting JavaScript's string literal options.
+// var str1 = "This works";
+// var str2 = 'This also works';
+// var str3 = `And this works as well`;
+
+// console.log(str1);
+// console.log(str2);
+// console.log(str3);
+
+// // Defines a string variable with escaped and unescaped quotes and logs it to console.
+//  var correct = `I can include '' and "" easily`
+//  console.log(correct);
+
+// // Logs a string containing an error message to the console.
+// var wrong = "I can not include " here";
+// console.log(wrong);
+
+// // Object creation with properties accessed via dot and bracket notation.
+// var obj = {
+//     name: 'Rhein',
+//     age: 22,
+//     isPopular: true,
+// }
+
+// console.log(obj.name);
+// console.log(obj['age']);
+
+// // Logs the data types of variables: number, string, object, and boolean.
+// var num = 2;
+// var str ="A string";
+// var obj = {
+//     name: "Tesla",
+//     sucks: true
+// };
+
+// var bool = false;
+
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof obj);
+// console.log(typeof bool);
+
+
+// // Logs the type of a null variable.
+// var obj =null
+
+// console.log(typeof obj);
+
+// // Logs the type and value of the array `arr`.
+// var arr = [1,2,3,4]
+
+// console.log(typeof arr);
+// console.log(arr);
+
+// var arr = [2, null, "Hello", { name: "Rhein"}]
+
+// console.log(typeof arr);
+// console.log(arr);
+
+// // Logging an undefined variable and its type.
+// var noValue;
+
+// console.log("Value is: " + noValue);
+// console.log("Type of: " + typeof noValue);
